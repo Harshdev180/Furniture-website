3f087e5c76050117d07d4ee2d2e656b967c71e03
Harshdev <harshdevsingh180@gmail.com>
2025-12-15 20:07:37 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-4xl text-red-500 p-6 justify-center mx-auto flex'>
+      <h1>Welcome to the Furniture Website</h1>
+    </div>
+  )
+}
+
+export default App
