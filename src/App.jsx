31039ca4d9612f314c102a1f7846d54be343b58e
Deepak2004Bisht <deepakbisht2777@gmail.com>
2025-12-17 21:17:43 +0530

    Home-Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Categories from "./components/Categories";
+// import Categories from "./components/Categories";
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/categories" element={<Categories />} />
-      <Route path="*" element={<h1>404 Page Not Found</h1>} />
-    </Routes>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} /> */}
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
