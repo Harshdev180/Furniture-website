5a25c3a19fd7d84738bb8eaa0bffce1336fb3208
Harshdev <harshdevsingh180@gmail.com>
2025-12-18 10:37:42 +0530

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Categories from "./components/Categories";
 import CategoryProducts from "./components/CategoryProducts";
 import CatelogPage from "./components/Catelog";
-import "./App.css";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/catalog" element={<CatelogPage />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/category/:type" element={<CategoryProducts />} />
-      <Route path="*" element={<h1>404 Page Not Found</h1>} />
-    </Routes>
-=======
-import { Routes, Route, Navigate } from "react-router-dom";
-// import Categories from "./components/Categories";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 import Home from "./components/home/Home";
+import "./App.css";
 
 function App() {
   return (
@@ -26,14 +13,20 @@ function App() {
       <Navbar />
 
       <Routes>
+        {/* Home page */}
         <Route path="/" element={<Home />} />
-        {/* <Route path="/categories" element={<Categories />} />
-        <Route path="*" element={<h1>404 Page Not Found</h1>} /> */}
+
+        {/* Catalog and categories pages */}
+        <Route path="/catalog" element={<CatelogPage />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:type" element={<CategoryProducts />} />
+
+        {/* 404 fallback */}
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
 
       <Footer />
     </>
->>>>>>> origin/Home-Page
   );
 }
 
