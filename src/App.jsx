9911d887abc040a9078b3293f9629cdf966f755e
Harshdev <harshdevsingh180@gmail.com>
2025-12-18 10:08:42 +0530

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import CatelogPage from './components/Catelog';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Categories from "./components/Categories";
+import CategoryProducts from "./components/CategoryProducts";
+import CatelogPage from "./components/Catelog";
+import "./App.css";
 
 function App() {
   return (
-   <>
-<CatelogPage/>
-   </>
-  )
+    <Routes>
+      <Route path="/catalog" element={<CatelogPage />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/category/:type" element={<CategoryProducts />} />
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
