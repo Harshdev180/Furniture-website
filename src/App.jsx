e847aef0427c7fda77f343be95eb3a579cd64d78
Deepak2004Bisht <deepakbisht2777@gmail.com>
2025-12-19 13:56:30 +0530

    Finished Home and Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 import Home from "./components/home/Home";
+import Contact from "./pages/contact/Contact";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
   return (
     <>
+      <ScrollTop />
+      
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
         {/* <Route path="/categories" element={<Categories />} />
         <Route path="*" element={<h1>404 Page Not Found</h1>} /> */}
       </Routes>
